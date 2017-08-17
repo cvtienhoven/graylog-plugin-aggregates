f5cf4f4e111e3eca3af0fd3f8e7de4346ac0ee5d
Christiaan van Tienhoven <tienhoven.c@tkppensioen.nl>
2017-08-17 09:18:04 +0200

    Discarded tabular layout in description because of issues in plain text email markup

--- src/main/java/org/graylog/plugins/aggregates/util/AggregatesUtil.java
+++ src/main/java/org/graylog/plugins/aggregates/util/AggregatesUtil.java
@@ -34,31 +34,14 @@ public class AggregatesUtil {
 
         final StringBuilder sb = new StringBuilder();
 
-        sb.append("Matched values for field [ " + rule.getField() + " ]\n\n");
-
-        int nameLength = 4;
-        int occurrencesLength = 11;
+        sb.append("Matched values for field [ " + rule.getField() + " ]\n");
 
         for (Map.Entry<String, Long> entry : matchedTerms.entrySet()) {
-            if (entry.getKey().length() > nameLength) {
-                nameLength = entry.getKey().length();
-            }
-            if (entry.getValue().toString().length() > occurrencesLength) {
-                occurrencesLength = entry.getValue().toString().length();
-            }
-        }
-
-        sb.append(String.format("%-" + (nameLength + 4) + "s", "FIELD").replace(' ', '_'));
-        sb.append(String.format("%-" + (occurrencesLength + 4) + "s", "#OCCURRENCES").replace(' ', '_'));
-        if (!emailConfiguration.isEnabled()) {
-            sb.append("\n");
-        } else {
-            sb.append(String.format("%-" + (occurrencesLength + 4) + "s", "#STREAM URL") + "\n");
-        }
-
-        for (Map.Entry<String, Long> entry : matchedTerms.entrySet()) {
-            sb.append(String.format("%-" + (nameLength + 4) + "s", entry.getKey()));
-            sb.append(String.format("%-" + (occurrencesLength + 4) + "s", entry.getValue()));
+        	
+        	sb.append("\nValue: " + entry.getKey() + "\n");
+            sb.append("Occurrences: " + entry.getValue() + "\n");
+            
+            
 
             if (!emailConfiguration.isEnabled()) {
                 sb.append("\n");
@@ -70,7 +53,7 @@ public class AggregatesUtil {
                     search_uri += "/streams/" + streamId;
                 }
                 search_uri += "/search?rangetype=absolute&fields=message%2Csource%2C" + rule.getField() + "&from=" + timeRange.getFrom() + "&to=" + timeRange.getTo() + "&q=" + URLEncoder.encode(rule.getQuery() + " AND " + rule.getField() + ":\"" + entry.getKey() + "\"", "UTF-8");
-                sb.append(emailConfiguration.getWebInterfaceUri() + search_uri);
+                sb.append("Search: " + emailConfiguration.getWebInterfaceUri() + search_uri + "\n");
 
             }
         }
